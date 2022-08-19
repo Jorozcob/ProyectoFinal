fb1390bae12d322f9d860ae855b81153457ebc45
Jorozcob <jorozcob1@miumg.edu.gt>
2022-08-19 15:22:09 -0600

    first commit

---
+++ function.py
@@ -0,0 +1,18 @@
+
+
+
+def model(modelo):
+    print("Ingresa el modelo")
+    modelo = input();
+    
+    if (modelo <1980):
+        print("Valor de retorno 110")
+
+    if ((modelo >1980) and (modelo < 1990)):
+        print("Valor de retorno 110")
+    if (modelo <1980):
+        print("Valor de retorno 110")
+    if (modelo <1980):
+        print("Valor de retorno 110")
+    if (modelo <1980):
+        print("Valor de retorno 110")
